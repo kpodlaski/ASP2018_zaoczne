d7267561a240124d7a9e02e9bf3280fa581965ec
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2018-04-21 17:07:49 +0200

    bazowa animacja

---
+++ Kulki/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kulki
+{
+    public class Ball
+    {
+        public float X { private set; get;}
+        public float Y { private set; get;}
+        public float Vx { private set; get; }
+        public float Vy { private set; get; }
+        private Thread thread;
+        static Random rand = new Random();
+
+        public Ball()
+        {
+            X = rand.Next() % 500 + 100;
+            Y = rand.Next() % 500 + 100;
+            Vy = (float) (rand.NextDouble() * 10 - 5);
+            Vx = (float)(rand.NextDouble() * 10 - 5);
+            thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    move();
+                    Thread.Sleep(30);
+                }
+            });
+        }
+
+        public void move()
+        {
+            X += Vx;
+            Y += Vy;
+        }
+
+        public void Start()
+        {
+            thread.Start();
+        }
+
+    }
+}
---
+++ Kulki/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kulki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Ball ball1 = new Ball();
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateScreen();
+        }
+
+        public void updateScreen()
+        {
+            Canvas.SetTop(EBall1, ball1.Y);
+            Canvas.SetLeft(EBall1, ball1.X);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Threading.DispatcherTimer dispatcherTimer =
+                new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            dispatcherTimer.Start();
+            ball1.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            updateScreen();
+            //ball1.move();
+        }
+    }
+}
